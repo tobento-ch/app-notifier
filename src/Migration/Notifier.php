debe93cae962fcbeda7afbd817abe1e18aa805d5
TOBENTOch <info@tobento.ch>
2024-02-21 17:14:39 +0100

    type and desc added

--- src/Migration/Notifier.php
+++ src/Migration/Notifier.php
@@ -67,6 +67,8 @@ class Notifier implements MigrationInterface
         return new Actions(
             new FilesCopy(
                 files: $this->files,
+                type: 'config',
+                description: 'Notifier config file.',
             ),
         );
     }
@@ -79,7 +81,11 @@ class Notifier implements MigrationInterface
     public function uninstall(): ActionsInterface
     {
         return new Actions(
-            new FilesDelete($this->files),
+            new FilesDelete(
+                files: $this->files,
+                type: 'config',
+                description: 'Notifier config file.',
+            ),
         );
     }
 }
