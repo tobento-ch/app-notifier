8ce3b598622b0881e2bdf04b6c0c10f2a8442648
TOBENTOch <info@tobento.ch>
2024-03-09 12:35:55 +0100

    interfaces implemented

--- src/Boot/Notifier.php
+++ src/Boot/Notifier.php
@@ -19,6 +19,12 @@ use Tobento\App\Migration\Boot\Migration;
 use Tobento\App\Mail\Boot\Mail;
 use Tobento\App\Queue\Boot\Queue;
 use Tobento\App\Database\Boot\Database;
+use Tobento\App\Notifier\AvailableChannelsInterface;
+use Tobento\App\Notifier\AvailableChannels;
+use Tobento\App\Notifier\Storage\NotificationFactoryInterface;
+use Tobento\App\Notifier\Storage\NotificationFactory;
+use Tobento\App\Notifier\Storage\NotificationFormattersInterface;
+use Tobento\App\Notifier\Storage\NotificationFormatters;
 use Tobento\Service\Notifier\NotifierInterface;
 use Tobento\Service\Notifier\Notifier as ServiceNotifier;
 use Tobento\Service\Notifier\ChannelsInterface;
@@ -78,14 +84,27 @@ class Notifier extends Boot
         );
         
         $this->app->set(NotifierInterface::class, ServiceNotifier::class);
-
-        // install migration after channels:
-        /*$migrator = $this->app->get(\Tobento\Service\Migration\MigratorInterface::class);
-        $installed = $migrator->install(\Tobento\App\Notifier\Migration\Storages::class);
-        echo '<pre>';
-        print_r($installed->actions());
-        exit;*/
         
+        $this->app->set(
+            AvailableChannelsInterface::class,
+            static function(ChannelsInterface $channels): AvailableChannelsInterface {
+                return new AvailableChannels($channels->names());
+            }
+        );
+
+        $this->app->set(NotificationFactoryInterface::class, NotificationFactory::class);
+        
+        $this->app->set(
+            NotificationFormattersInterface::class,
+            static function(ContainerInterface $container) use ($config): NotificationFormattersInterface {
+                return new NotificationFormatters(
+                    container: $container,
+                    formatters: $config['formatters'] ?? []
+                );
+            }
+        );
+
+        // install migration after channels:        
         $migration->install(\Tobento\App\Notifier\Migration\Storages::class);
     }
 }
